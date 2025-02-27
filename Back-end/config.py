cd50ddf1a26a7c2748f762ccd9be51abe77c65d9
Gabriel-dev001 <gabriel.developer103@gmail.com>
2025-02-27 20:48:12 -0300

    Removendo arquivos ignorados pelo .gitignore

--- Back-end/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "sua_chave_secreta_super_segura")
-    DEBUG = True  
-
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/bd_bankcap'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
